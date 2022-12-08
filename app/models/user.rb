4cbaf9017a59169878e295f25eaa8b5b07d99e08
Tadesse Jemal <tadessejemal63@gmail.com>
2022-12-08 16:20:00 +0300

    fix sign_up problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ApplicationRecord
   has_many :likes
 
   # Name must not be blank.
-  validates :name, :photo, :bio, presence: true
+  validates :name, presence: true
 
   # PostsCounter must be an integer greater than or equal to zero.
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_post(id)
     Post.order(created_at: :desc).where(author_id: id).first(3)
