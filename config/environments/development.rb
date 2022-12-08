291593aceeddcbba22e5218f899920e556a05355
angeluray <aeucampos@gmail.com>
2022-12-08 15:14:28 -0400

    Fix letter opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,14 +32,19 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-
+  
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
-
+  
   # Don't care if the mailer can't send.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
   config.action_mailer.raise_delivery_errors = false
-
+  
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -59,9 +64,6 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
   
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = true
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
