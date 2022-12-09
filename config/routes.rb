e8a52eb62d262255d9d04e2a81f580d8af6a911e
Tadesse Jemal <tadessejemal63@gmail.com>
2022-12-09 22:28:46 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,15 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: %i[index show] do
+        resources :posts, only: %i[index show] do
+          resources :comments, only: %i[index create]
+        end
+      end
+    end
+  end
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
       resources :comments, only: [:new, :create, :destroy]
