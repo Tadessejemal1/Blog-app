d7598e8c706630bcf2253a239bab3142fbd89066
Tadesse Jemal <tadessejemal63@gmail.com>
2022-12-10 18:11:47 +0300

    fix linter

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,18 +1,18 @@
 class Api::V1::CommentsController < ApplicationController
-    # protect_from_forgery with: :null_session
-  
-    def index
-        @post = Post.find(params[:post_id])
-        @comments = @post.comments.order(created_at: :desc)
-        render json: @comments
-    end
-  
-    def create
-        input_values = params.require(:comment).permit(:text)
-        @comment = Comment.new(text: input_values[:text], author_id: current_user.id, post_id: params[:post_id])
-    
-        if @comment.save
-          render json: @comment, status: :created, location: @comment
-        end
-    end
+  # protect_from_forgery with: :null_session
+
+  def index
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments.order(created_at: :desc)
+    render json: @comments
   end
+
+  def create
+    input_values = params.require(:comment).permit(:text)
+    @comment = Comment.new(text: input_values[:text], author_id: current_user.id, post_id: params[:post_id])
+
+    return unless @comment.save
+
+    render json: @comment, status: :created, location: @comment
+  end
+end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::UsersController < ApplicationController
-    # before_action :authorize_request
-    def index
-      @users = User.all
-      render json: @users, status: :ok
-    end
-  
-    def show
-      user = User.find(params[:id])
-      render json: user, status: :ok
-    end
+  # before_action :authorize_request
+  def index
+    @users = User.all
+    render json: @users, status: :ok
+  end
+
+  def show
+    user = User.find(params[:id])
+    render json: user, status: :ok
+  end
 end
